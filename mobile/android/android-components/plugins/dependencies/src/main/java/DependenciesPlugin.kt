7ee83e565b2040785b238897903d42307af11cf6
Jamie Nicol <jnicol@mozilla.com>
2024-09-11 10:35:15 +0100

    downgrade android gradle plugin
    this allows us to build in android studio jellyfish

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -30,7 +30,7 @@ object Versions {
     const val okio = "3.9.0"
     const val androidsvg = "1.4"
 
-    const val android_gradle_plugin = "8.6.0"
+    const val android_gradle_plugin = "8.4.2"
 
     // This has to be synced to the gradlew plugin version. See
     // http://googlesamples.github.io/android-custom-lint-rules/api-guide/example.md.html#example:samplelintcheckgithubproject/lintversion?
